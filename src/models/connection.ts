6cf134993c2ff4cbaed68d15794e371031f3240a
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2022-12-14 17:06:09 -0300

    refactor: Delete database key of connection

--- src/models/connection.ts
+++ src/models/connection.ts
@@ -1,11 +1,10 @@
-import mySql from 'mysql2';
+import mySql from 'mysql2/promise';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 export default mySql.createPool({
-  host: process.env.DB_HOSTNAME || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '12345678',
-  database: process.env.DB_DATABASE || 'Trybesmith',
+  host: process.env.MYSQL_HOST || 'localhost', 
+  user: process.env.MYSQL_USER || 'root',
+  password: process.env.MYSQL_PASSWORD || 'password',
 }); 
