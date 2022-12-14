93e1ac77110812be3936ecd5d1fd04a1ef0ed4a5
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2022-12-14 20:38:25 -0300

    feat: Add login router in app file

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import productsRouter from './routes/products.router';
 import usersRouter from './routes/users.router';
 import ordersRouter from './routes/orders.router';
+import loginRouter from './routes/login.router';
 
 const app = express();
 
@@ -9,5 +10,6 @@ app.use(express.json());
 app.use('/products', productsRouter);
 app.use('/users', usersRouter);
 app.use('/orders', ordersRouter);
+app.use('/login', loginRouter);
 
 export default app;
