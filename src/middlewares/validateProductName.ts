b441dd13c097d5f240da40dac28daeda76f7955b
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2022-12-15 11:41:35 -0300

    feat: Create middleware for validade product name field

---
+++ src/middlewares/validateProductName.ts
@@ -0,0 +1,24 @@
+import { NextFunction, Request, Response } from 'express';
+import statusCodes from '../utils/statusCodes';
+
+export default class ValidateProductName {
+  public validateName = (req: Request, res: Response, next: NextFunction): void | Response => {
+    if (!('name' in req.body)) {
+      return res.status(statusCodes.BAD_REQUEST).json({ message: '"name" is required' });
+    }
+
+    const { name } = req.body;
+
+    if (typeof name !== 'string') {
+      return res.status(statusCodes.UNPROCESSABLE_ENTITY)
+        .json({ message: '"name" must be a string' });
+    }
+
+    if (name.length > 2) {
+      return res.status(statusCodes.UNPROCESSABLE_ENTITY)
+        .json({ message: '"name" length must be at least 3 characters long' });
+    }
+
+    next();
+  };
+}
