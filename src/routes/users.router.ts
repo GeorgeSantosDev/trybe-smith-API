f0c07f7f99a48e9741f9e9d3f07b36b5686fe0f1
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2022-12-15 11:54:06 -0300

    feat: Add username validation in users route

--- src/routes/users.router.ts
+++ src/routes/users.router.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import UsersController from '../controllers/users.controller';
+import ValidateUsername from '../middlewares/validateUsername';
 
 const controller = new UsersController();
+const { validateUsername } = new ValidateUsername();
 
 const router = Router();
 
-router.post('/', controller.create);
+router.post('/', validateUsername, controller.create);
 
 export default router;
