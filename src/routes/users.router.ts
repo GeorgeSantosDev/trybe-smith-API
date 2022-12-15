36d78b01bce8b2f19ac4e8562342e0c03277430a
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2022-12-15 12:02:01 -0300

    feat: Add validateLevel in users route

--- src/routes/users.router.ts
+++ src/routes/users.router.ts
@@ -1,14 +1,16 @@
 import { Router } from 'express';
 import UsersController from '../controllers/users.controller';
+import ValidateLevel from '../middlewares/validateLevel';
 import ValidateUsername from '../middlewares/validateUsername';
 import ValidateVocation from '../middlewares/validateVocation';
 
 const controller = new UsersController();
 const { validateUsername } = new ValidateUsername();
 const { validateVocation } = new ValidateVocation();
+const { validateLevel } = new ValidateLevel();
 
 const router = Router();
 
-router.post('/', validateUsername, validateVocation, controller.create);
+router.post('/', validateUsername, validateVocation, validateLevel, controller.create);
 
 export default router;
