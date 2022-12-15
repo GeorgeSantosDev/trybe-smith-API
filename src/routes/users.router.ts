1a73df44853a4b14bedeb995de8e789255b10680
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2022-12-15 12:06:00 -0300

    feat: Add middleware for validate password in users route

--- src/routes/users.router.ts
+++ src/routes/users.router.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import UsersController from '../controllers/users.controller';
 import ValidateLevel from '../middlewares/validateLevel';
+import ValidatePassword from '../middlewares/validatePassword';
 import ValidateUsername from '../middlewares/validateUsername';
 import ValidateVocation from '../middlewares/validateVocation';
 
@@ -8,9 +9,17 @@ const controller = new UsersController();
 const { validateUsername } = new ValidateUsername();
 const { validateVocation } = new ValidateVocation();
 const { validateLevel } = new ValidateLevel();
+const { validatePassword } = new ValidatePassword();
 
 const router = Router();
 
-router.post('/', validateUsername, validateVocation, validateLevel, controller.create);
+router.post(
+  '/',
+  validateUsername,
+  validateVocation,
+  validateLevel,
+  validatePassword,
+  controller.create,
+);
 
 export default router;
