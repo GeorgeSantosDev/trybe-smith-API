4d2de83543ed2aedeebee62029343c1bd947217e
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2022-12-15 11:50:16 -0300

    feat: Add validateAmount in product route

--- src/routes/products.router.ts
+++ src/routes/products.router.ts
@@ -1,13 +1,15 @@
 import { Router } from 'express';
 import ProductsController from '../controllers/products.controller';
 import ValidateProductName from '../middlewares/validateProductName';
+import ValidateProductAmount from '../middlewares/validateProductAmount';
 
 const controller = new ProductsController();
 const { validateName } = new ValidateProductName();
+const { validateAmount } = new ValidateProductAmount();
 
 const router = Router();
 
 router.get('/', controller.getAll);
-router.post('/', validateName, controller.create);
+router.post('/', validateName, validateAmount, controller.create);
 
 export default router;
