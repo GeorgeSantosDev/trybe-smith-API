21e5415db9572e2cb4e99347a31b401d0685ffbf
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2022-12-15 11:44:04 -0300

    feat: Add validate product name middleware in product route

--- src/routes/products.router.ts
+++ src/routes/products.router.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import ProductsController from '../controllers/products.controller';
+import ValidateProductName from '../middlewares/validateProductName';
 
 const controller = new ProductsController();
+const { validateName } = new ValidateProductName();
 
 const router = Router();
 
 router.get('/', controller.getAll);
-router.post('/', controller.create);
+router.post('/', validateName, controller.create);
 
 export default router;
