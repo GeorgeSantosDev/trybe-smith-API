daa5941daf0e85dcaf2e3a065f6a4751e9597b86
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2022-12-15 11:57:35 -0300

    feat: Add middleware for validate vocation field

--- src/routes/users.router.ts
+++ src/routes/users.router.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express';
 import UsersController from '../controllers/users.controller';
 import ValidateUsername from '../middlewares/validateUsername';
+import ValidateVocation from '../middlewares/validateVocation';
 
 const controller = new UsersController();
 const { validateUsername } = new ValidateUsername();
+const { validateVocation } = new ValidateVocation();
 
 const router = Router();
 
-router.post('/', validateUsername, controller.create);
+router.post('/', validateUsername, validateVocation, controller.create);
 
 export default router;
