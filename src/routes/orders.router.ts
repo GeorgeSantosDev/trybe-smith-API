371b38f52c8ef2c218ee20b6ffcfc5d2518b9f22
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2022-12-15 22:15:38 -0300

    feat: Add post route in orders routes file

--- src/routes/orders.router.ts
+++ src/routes/orders.router.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express';
 import OrdersController from '../controllers/orders.controller';
+import ValidateProductsIdsField from '../middlewares/validateProductsIdsField';
+import { verifyToken } from '../auth/jwt';
 
 const controller = new OrdersController();
+const { validateProductId } = new ValidateProductsIdsField();
 
 const router = Router();
 
 router.get('/', controller.getAll);
+router.post('/', verifyToken, validateProductId, controller.create);
 
 export default router;
